8706ba7fda57d9766dd5f846b77559f82910c976
Shao tsziatsi <shaots@mail.ru>
2024-06-08 23:41:47 +0300

    Rename members of class, adding prefix m_

--- Solver.cpp
+++ Solver.cpp
@@ -26,20 +26,20 @@ std::array<Point, Solver::numPoints> Solver::gradMethod(double residual) {
     // Условие внутреннего цикла
     bool flag2 = true;
     do {
-        alpha = alpha0;
+        alpha = m_alpha0;
         do {
-            std::array<Point, numPoints> grad1 = gradient(ABC1[0], ABC1[1], ABC1[2]);
+            std::array<Point, numPoints> grad1 = problem.gradient(ABC1[0], ABC1[1], ABC1[2]);
 
             ABC2[0] = Point::difference(ABC1[0], Point::multiply(grad1[0], alpha));
             ABC2[1] = Point::difference(ABC1[1], Point::multiply(grad1[1], alpha));
             ABC2[2] = Point::difference(ABC1[2], Point::multiply(grad1[2], alpha));
-            flag2 = Fun(ABC2[0], ABC2[1], ABC2[2]) -
-                    Fun(ABC1[0], ABC1[1], ABC1[2]) >
-                    -alpha * delta * norm2Square(grad1);
-            alpha *= lambda;
+            flag2 = problem.Fun(ABC2[0], ABC2[1], ABC2[2]) -
+                    problem.Fun(ABC1[0], ABC1[1], ABC1[2]) >
+                    -alpha * m_delta * norm2Square(grad1);
+            alpha *= m_lambda;
         } while (flag2);
-        std::array<Point, numPoints> grad2 = gradient(ABC2[0], ABC2[1], ABC2[2]);
-        flag1 = norm2Square(grad2) > residual * residual;
+        std::array<Point, numPoints> grad2 = problem.gradient(ABC2[0], ABC2[1], ABC2[2]);
+        flag1 = norm2Square(grad2) > m_residual * m_residual;
         ABC1 = ABC2;
     } while (flag1);
     return ABC2;
